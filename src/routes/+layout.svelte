c0c497351782321d42f39c5d8e7c865c024fc159
oxy-k <oxy.v.bee@gmail.com>
2024-08-23 07:06:23 +0300

    Run format

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,7 +20,7 @@
 		display: flex;
 		flex-direction: column;
 		width: 100%;
-        height: 100%;
+		height: 100%;
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
--- src/routes/api/image/+server.ts
+++ src/routes/api/image/+server.ts
@@ -1,22 +1,22 @@
-import OpenAI from "openai"
+import OpenAI from 'openai';
 import { OPENAI_API_KEY } from '$env/static/private';
 
 const client = new OpenAI({
 	apiKey: OPENAI_API_KEY
-})
+});
 
 async function getImage(prompt: string) {
 	return client.images.generate({
-		model: "dall-e-3",
+		model: 'dall-e-3',
 		prompt: prompt,
 		n: 1,
-		size: "1024x1024",
-	})
+		size: '1024x1024'
+	});
 }
 
 export async function POST({ request }) {
 	const { prompt } = await request.json();
-	const response = await getImage(prompt || "a city view")
+	const response = await getImage(prompt || 'a city view');
 	const image_url = response.data[0].url;
 
 	return new Response(JSON.stringify({ imgSrc: image_url }), {
@@ -26,4 +26,3 @@ export async function POST({ request }) {
 		status: 200
 	});
 }
-
